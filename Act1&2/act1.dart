c1ee02aa681a52e6134facd5234ef9f3ccd22b4d
loocnathaniel <nathaniellooc15@gmail.com>
2025-08-07 09:55:34 +0000

    act1

---
+++ Act1&2/act1.dart
@@ -0,0 +1,11 @@
+// // void main() {
+// //   dynamic name = 'Alice';
+// //   print("Welcome, $name!");
+// // }
+ 
+// void main(){
+//   dynamic name = 'Alice';
+//   name = 25;
+//   print("Welcome, $name!");
+// }
+// the dynamic can hold any data type if u use this it will not cause a error
---
+++ Act1&2/act2.dart
@@ -0,0 +1,19 @@
+void main() {
+double result = calculatePerimeter(7, 0, true);
+  print('The perimeter of  square is  ' + result.toString());
+  
+result = calculatePerimeter(10, 5, false);
+   print('The perimeter of rectangle is  ' + result.toString());
+ }
+
+
+double calculatePerimeter(double width, double height, bool isSquare) {
+double perimeter;
+ if(isSquare)
+{
+  perimeter = 4 * width;
+ } else{
+     perimeter = 2 * (width + height);
+   }
+   return perimeter;
+ }
